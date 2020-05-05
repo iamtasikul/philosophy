65b181c3281837a8c21585feb79926136c1a9f9f
Tasikul Islam <iamtasikul@gmail.com>
2020-05-05 14:10:25 +0530

    18.8 Blog Home Page Pagination

--- functions.php
+++ functions.php
@@ -36,3 +36,19 @@ function philosophy_assets()
     wp_enqueue_script("main-js", get_theme_file_uri("/assets/js/main.js"), array("jquery"), "1.0", true);
 }
 add_action("wp_enqueue_scripts", "philosophy_assets");
+
+function philosophy_pagination()
+{
+    global $wp_query;
+    $links = paginate_links(array(
+        'current'  => max(1, get_query_var('paged')),
+        'total'    => $wp_query->max_num_pages,
+        'type'     => 'list',
+        'mid_size' => apply_filters("philosophy_pagination_mid_size", 3)
+    ));
+    $links = str_replace("page-numbers", "pgn__num", $links);
+    $links = str_replace("<ul class='pgn__num'>", "<ul>", $links);
+    $links = str_replace("next pgn__num", "pgn__next", $links);
+    $links = str_replace("prev pgn__num", "pgn__prev", $links);
+    echo $links;
+}
