38d4a57995ba47b7be5000ce2aeeec910d16168b
Tasikul Islam <iamtasikul@gmail.com>
2020-05-05 16:48:02 +0530

    18.12 Single Post

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 
     <!-- pageheader
     ================================================== -->
-    <section class="s-pageheader s-pageheader--home">
+    <section class="s-pageheader <?php if (is_home()) echo "s-pageheader--home"; ?>">
 
         <header class="header">
             <div class="header__content row">
@@ -68,8 +68,10 @@
 
             </div> <!-- header-content -->
         </header> <!-- header -->
-
-
-        <?php get_template_part("/template-parts/blog-home/featured"); ?>
+        <?php
+        if (is_home()) {
+            get_template_part("/template-parts/blog-home/featured");
+        }
+        ?>
 
     </section> <!-- end s-pageheader -->
