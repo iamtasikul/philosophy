2b04a2a73432c9d2904e133caa4f051cfabde0df
Tasikul Islam <iamtasikul@gmail.com>
2020-05-06 07:41:33 +0530

    18.18 Tags in footer section

--- footer.php
+++ footer.php
@@ -66,17 +66,7 @@
             <h3>Tags</h3>
 
             <div class="tagcloud">
-                <a href="#0">Salad</a>
-                <a href="#0">Recipe</a>
-                <a href="#0">Places</a>
-                <a href="#0">Tips</a>
-                <a href="#0">Friends</a>
-                <a href="#0">Travel</a>
-                <a href="#0">Exercise</a>
-                <a href="#0">Reading</a>
-                <a href="#0">Running</a>
-                <a href="#0">Self-Help</a>
-                <a href="#0">Vacation</a>
+                <?php the_tags('', '', '') ?>
             </div> <!-- end tagcloud -->
         </div> <!-- end tags -->
     </div> <!-- end tags-wrap -->
